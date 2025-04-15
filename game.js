f136d280c00bfce9f19ea8a45e34c2e64229294c
qejmer <unaymazlum@gmail.com>
2025-04-15 00:36:33 -0400

    add start game feature

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 var buttonColours=["red", "blue", "green", "yellow"];
 var gamePattern=[];
 var userClickedPatern = [];
-$(".btn").click(function(){
-    var userChosenColour = $(this).attr("id");
-    userClickedPatern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-    console.log(userClickedPatern);
-});
+var level = 0;
+var started = false;
+
 
 
 function nextSequence(){
+
+    var userClickedPatern = [];      // Reset user pattern for new level
+
+     // Increase level and update display
+     level++;
+     $("#level-title").text("Level " + level);
 
     var randomNumber =  Math.floor(Math.random()*4);
     
@@ -22,8 +24,15 @@ function nextSequence(){
 
     playSound(randomChosenColour);
            
-
 };
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPatern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
 
 function playSound(name){
     var audio = new Audio("./sounds/"+name+".mp3").play();
@@ -37,7 +46,14 @@ function animatePress(currentColour){
     },100)
 }
 
-nextSequence();
+$(document).keypress(function() {
+    if (!started) {
+
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
 
 
 
